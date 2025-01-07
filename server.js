e00bac581e90850ea4df44f87717dd1bcee63b02
z2024z <193938894+z2025z01070110@users.noreply.github.com>
2025-01-07 21:14:36 +0800

    lops

--- server.js
+++ server.js
@@ -5,3 +5,26 @@ const db = new sqlite3.Database('union_lotto.db', (err) => {
     }
     console.log('Connected to the union_lotto database.');
 });
+
+const createTable = () => {
+    const sql = `
+        create table if not exists union_lotto(
+            id integer primary key autoincrement,
+            date date not null,
+            red1 integer not null,
+            red2 integer not null,
+            red3 integer not null,
+            red4 integer not null,
+            red5 integer not null,
+            red6 integer not null,
+            blue integer not null
+        )
+    `
+    db.run(sql, (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Created table union_lotto.');
+    });
+}
+createTable();
