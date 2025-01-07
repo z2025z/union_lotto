d8a3c2e18c695fa73112afffdd6be1097f19f9b1
z2024z <193938894+z2025z01070110@users.noreply.github.com>
2025-01-07 16:33:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('union_lotto.db',(err)=>{
-    if(err){
+const db = new sqlite3.Database('union_lotto.db', (err) => {
+    if (err) {
         return console.error(err.message);
     }
     console.log('Connected to the union_lotto database.');
